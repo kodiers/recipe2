e78c9006ba0dc3dc39bdb31b75167a0a88b150f9
VY <kodiers@gmail.com>
2017-10-17 03:49:40 +0300

    Work on shoppinglist service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,36 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg')
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+            'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg', [
+                new Ingredient('meat', 1),
+                new Ingredient('French fries', 20)
+            ]),
+        new Recipe(
+            'Another Test Recipe',
+            'This is simply a test',
+            'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg', [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
     ];
 
     recipeSelected = new EventEmitter<Recipe>();
 
+    constructor(private slService: ShoppingListService) {}
+
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
