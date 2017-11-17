b8da9119560d23feb11432053f5432159ec218db
VY <kodiers@gmail.com>
2017-11-17 03:42:29 +0300

    Some cleanup

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
       this.ingredients = this.shoppingListService.getIngredients();
-      this.shoppingListService.ingredientsChange.subscribe(
+      this.subscription = this.shoppingListService.ingredientsChange.subscribe(
           (ingredients: Ingredient[]) => {
               this.ingredients = ingredients;
           }
@@ -23,6 +25,10 @@ export class ShoppingListComponent implements OnInit {
 
   onIngredientAdded(ingredient: Ingredient) {
       this.ingredients.push(ingredient);
+  }
+
+  ngOnDestroy() {
+      this.subscription.unsubscribe();
   }
 
 }
