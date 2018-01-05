241f135b414cf1f154316a167e0b4925b963f9cc
Victor Yamchinov <kodiers@gmail.com>
2018-01-05 04:16:20 +0300

    Added interceptors

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams, HttpRequest} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 import 'rxjs/Rx';
@@ -11,13 +11,21 @@ export class DataStorageService {
   constructor(private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
 
   storeRecipes() {
-    const token = this.authService.getToken();
-    return this.httpClient.put('https://recipebook-b74df.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    // const token = this.authService.getToken();
+    // return this.httpClient.put('https://recipebook-b74df.firebaseio.com/recipes.json',
+    //   this.recipeService.getRecipes(), {
+    //   observe: 'body',
+    //   params: new HttpParams().set('auth', token)
+    //   // headers: new HttpHeaders().set('Authorization', 'Bearer')
+    // });
+    const req = new HttpRequest('PUT', 'https://recipebook-b74df.firebaseio.com/recipes.json',
+      this.recipeService.getRecipes(), {reportProgress: true});
+    return this.httpClient.request(req);
   }
 
   getRecipes() {
     const token = this.authService.getToken();
-    this.httpClient.get<Recipe[]>('https://recipebook-b74df.firebaseio.com/recipes.json?auth=' + token).map((recipes) => {
+    this.httpClient.get<Recipe[]>('https://recipebook-b74df.firebaseio.com/recipes.json').map((recipes) => {
       for (let recipe of recipes) {
         if (!recipe['ingredients']) {
           recipe['ingredients'] = [];
