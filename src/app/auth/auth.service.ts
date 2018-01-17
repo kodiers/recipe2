549651319a9f32f26acdb329f029f9c3381b9857
Victor Yamchinov <kodiers@gmail.com>
2018-01-17 03:46:43 +0300

    work with auth (ngrx)

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,24 +1,35 @@
 import * as firebase from 'firebase';
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
+import {Store} from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducers';
+import * as authActions from './store/auth.actions';
 
 @Injectable()
 export class AuthService {
   token: string;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private store: Store<fromApp.AppState>) {}
 
   signupUser(email: string, password: string) {
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(error => console.log(error));
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(
+      (user) => {
+        this.store.dispatch(new authActions.Signup());
+        firebase.auth().currentUser.getToken().then((token: string) => {
+          this.store.dispatch(new authActions.SetToken(token));
+        });
+      }
+    ).catch(error => console.log(error));
   }
 
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password).then(
       response => {
-        console.log(response);
+        this.store.dispatch(new authActions.Signin());
         this.router.navigate(['/']);
         firebase.auth().currentUser.getToken().then((token: string) => {
-          this.token = token;
+          this.store.dispatch(new authActions.SetToken(token));
         });
       }).catch(
       error => console.log(error)
@@ -30,14 +41,5 @@ export class AuthService {
       this.token = token;
     });
     return this.token;
-  }
-
-  isAuthenticated() {
-    return this.token != null;
-  }
-
-  logout() {
-    firebase.auth().signOut();
-    this.token = null;
   }
 }
